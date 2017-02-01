8d4f7ea197adcd10950b2f1ffad88555831adb40
christinamigliore <migliore.c@husky.neu.edu>
2017-01-31 20:45:56 -0500

    Update get_loop_cls.py

--- get_loop_cls.py
+++ get_loop_cls.py
@@ -37,29 +37,30 @@ def get_loop_cls(grid, inputted_parameter, roof=1e22):
     dx = np.diff(grid)
     dq_dx = dq/dx
 
-    if len(dq_dx[np.where(dq_dx == 0)]) == ngrid:
+    if len(dq_dx[np.where(dq_dx == 0)])+1 == ngrid:
         grid_array = np.zeros(ngrid)
-        characteristic_length_scale = [x + roof for x in grid_array]
+        characteristic_length_scale = [x + roof for x in grid_array] 
 
-    index_zero = np.argwhere(dq_dx == 0)
-    index_zero_parameter = np.argwhere(inputted_parameter == 0)
+    else:
+        index_zero = np.argwhere(dq_dx == 0)
+        index_zero_parameter = np.argwhere(inputted_parameter == 0)
 
-    zero_length = len(index_zero)
-    zero_parameter_length = len(index_zero_parameter)
+        zero_length = len(index_zero)
+        zero_parameter_length = len(index_zero_parameter)
+    
+        for i in range(0, zero_length):
+            dq_dx[index_zero[i]] = 1e-30
 
-    for i in range(0, zero_length):
-        dq_dx[index_zero[i]] = 1e-30
+        for j in range(0, zero_parameter_length):
+            inputted_parameter[index_zero_parameter[j]] = 1e-30
 
-    for j in range(0, zero_parameter_length):
-        inputted_parameter[index_zero_parameter[j]] = 1e-30
+        characteristic_length_scale = np.abs((dq_dx/inputted_parameter[: -1])**-1)
 
-    characteristic_length_scale = np.abs((dq_dx/inputted_parameter[: -1])**-1)
+        index_infinite = np.argwhere(characteristic_length_scale)
+        length_of_cls = len(characteristic_length_scale)
 
-    index_infinite = np.argwhere(characteristic_length_scale)
-    length_of_cls = len(characteristic_length_scale)
-
-    for h in range(0, length_of_cls):
-        if np.isfinite(characteristic_length_scale[h]) == False:
-            characteristic_length_scale[h] = roof
+        for h in range(0, length_of_cls):
+            if np.isfinite(characteristic_length_scale[h]) == False:
+                characteristic_length_scale[h] = roof
 
     return characteristic_length_scale 
