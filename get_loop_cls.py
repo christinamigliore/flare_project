5b067f9746abb561111863d6a95883ac8a19ead0
christinamigliore <migliore.c@husky.neu.edu>
2017-01-31 20:32:26 -0500

    Update get_loop_cls.py

--- get_loop_cls.py
+++ get_loop_cls.py
@@ -4,7 +4,7 @@ DOCUMENTATION
 PURPOSE
 -------
 	Calculates the characteristic length scale of the inputed grid
-	with the inputted parameter.
+	with the inputted parameter, (q).
 
 INPUTS
 ------
@@ -26,41 +26,40 @@ OUTPUTS
 """
 import numpy as np
 
-def test_cases(grid, inputted_parameter, roof=1e22):
-	"""
-	Function takes the derivative for each value, checks
-	if it equals 0 then converts values into the
-	characteristic length scale
-	"""
-	ngrid = len(grid)
-	characteristic_length_scale = []
-	for i in range(0, ngrid-1):
-		dq = inputted_parameter[0+i] - inputted_parameter[1+i]
-		dx = grid[0+i] - grid[1+i]
-		dq_dx = dq/dx
-		"""
-		if dq_dx != 0:
-			grid_array = np.zeros(ngrid)
-			characteristic_length_scale.append([x + roof for x in grid_array])
-		"""
-		if dq_dx != 0:
-			characteristic_length_scale.append(np.abs((dq_dx/inputted_parameter[i])**-1))
-		if dq_dx == 0:
-			dq_dx = 1e-30
-			characteristic_length_scale.append(np.abs((dq_dx/inputted_parameter[i])**-1))
-		if inputted_parameter[i] == 0:
-			inputted_parameter[i] = 1e-30
-			characteristic_length_scale.append(np.abs((dq_dx/inputted_parameter[i])**-1))
-	return np.asarray(characteristic_length_scale)
-
 def get_loop_cls(grid, inputted_parameter, roof=1e22):
-	"""
-	Function checks if the length scale is finite then outputs the final characteristic
-	length scales
-	"""
-	characteristic_length_scale = test_cases(grid, inputted_parameter, roof=1e22)
-	length_of_cls = len(characteristic_length_scale)
-	for i in range(0, length_of_cls):
-		if np.isfinite(characteristic_length_scale[i]) == False:
-			characteristic_length_scale[i] = roof
-	return characteristic_length_scale 
+    """
+    Function takes the derivative for each value, checks
+    if it equals 0 then converts values into the
+    characteristic length scale
+    """
+    ngrid = len(grid)
+    dq = np.diff(inputted_parameter)
+    dx = np.diff(grid)
+    dq_dx = dq/dx
+
+    if len(dq_dx[np.where(dq_dx == 0)]) == ngrid:
+        grid_array = np.zeros(ngrid)
+        characteristic_length_scale = [x + roof for x in grid_array]
+
+    index_zero = np.argwhere(dq_dx == 0)
+    index_zero_parameter = np.argwhere(inputted_parameter == 0)
+
+    zero_length = len(index_zero)
+    zero_parameter_length = len(index_zero_parameter)
+
+    for i in range(0, zero_length):
+        dq_dx[index_zero[i]] = 1e-30
+
+    for j in range(0, zero_parameter_length):
+        inputted_parameter[index_zero_parameter[j]] = 1e-30
+
+    characteristic_length_scale = np.abs((dq_dx/inputted_parameter[: -1])**-1)
+
+    index_infinite = np.argwhere(characteristic_length_scale)
+    length_of_cls = len(characteristic_length_scale)
+
+    for h in range(0, length_of_cls):
+        if np.isfinite(characteristic_length_scale[h]) == False:
+            characteristic_length_scale[h] = roof
+
+    return characteristic_length_scale 
