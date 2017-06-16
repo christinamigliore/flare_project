5d36c9f47bf057454ae7d0ff4ed784999fc4ae90
christinamigliore <migliore.c@husky.neu.edu>
2017-06-16 09:47:22 -0400

    Update random_generator.py

--- random_generator.py
+++ random_generator.py
@@ -1,34 +1,33 @@
-"""
-DOCUMENTATION
-
-PURPOSE
--------
-    Produces lists of randomly generated initial velocities and initial positions.
-
-INPUTS
-------
-    num_of_particles: integer
-        Number of particles to be generated
-        
-OUTPUTS
--------
-    particle_positions: list
-        List of initial x, y, and z velocities of particles.
-
-    particle_velocities: list
-        List of initial x, y, and z positions of particles.
-"""
 import numpy as np
 
-def get_particle_points(num_of_particles):
+def get_random_points(number_of_points, min_lim, max_lim, dimensions):
+    """
+    PURPOSE
+    -------
+        Produces lists of randomly generated vectors of specified
+        dimension and min/max values.
 
-    position_vector = []
-    velocity_vector = []
+    INPUTS
+    ------
+        num_of_particles: integer
+            Number of particles to be generated
 
-    for i in range(0, num_of_particles):
-        position = np.random.uniform(0, 1, 3)
-        velocity = np.random.uniform(10, 200, 3)
-        position_vector.append(position)
-        velocity_vector.append(velocity)
+        min_lim: integer
+            Minimum limit to the randomly generated value
 
-    return position_vector, velocity_vector
+        max_lim: integer
+            Maximum limit to the randomly generated value
+
+        dimensions: integer
+            Dimension of the randomly generated vector
+            
+    OUTPUTS
+    -------
+        vector: list
+            List of randomly generated vectors.
+    """
+    vector = []
+    for i in range(0, number_of_points):
+        rand_nums = np.random.uniform(min_lim, max_lim, dimensions)
+        vector.append(rand_nums)
+    return vector 
