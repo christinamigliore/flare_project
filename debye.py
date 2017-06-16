2c4a9d77a6966fc6413e8c26bef3f0dcf424c588
christinamigliore <migliore.c@husky.neu.edu>
2017-06-16 09:43:12 -0400

    Update debye.py

--- debye.py
+++ debye.py
@@ -7,16 +7,28 @@ import math
 def calculate_debye(electron_nd=10**32, ion_nd=10**32, electron_ke=2*10**6,
                     ion_ke=2*10**6):
     """
-    This function calculates the Debye length using the formula found in "The Physics of
+    Calculates the Debye length using the formula found in "The Physics of
     Solar Flares" from chapter 3, page 61.
-    Parameters:
-		ne = the electron number density
-		ni = the ion number density
-		Ti = ion kinetic energy
-		Te = electron kinetic energy
 
-    Returns:
-        debye = the Debye length (meters)
+    Parameters
+    ----------
+	    electron_nd : float (optional)
+	        Electron number density
+
+		electron_ke : float (optional)
+		    Electron kinetic energy
+
+		ion_nd : float (optional)
+		    Ion number density
+
+		ion_ke : float (optional)
+		    Ion kinetic energy
+
+    Returns
+    -------
+        debye : float
+            The Debye length (meters)
+
     """
     k_boltzmann = 1.38064852 * 10**-23
     electron_charge = 1.6021766208 * 10**-19
@@ -26,4 +38,3 @@ def calculate_debye(electron_nd=10**32, ion_nd=10**32, electron_ke=2*10**6,
 
 if __name__ == '__main__':
     print(calculate_debye())
-	
