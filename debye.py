bc5ebd23cd8bf01143300b52f0de7c06fe7b2976
christinamigliore <migliore.c@husky.neu.edu>
2017-11-07 19:55:52 -0500

    Update debye.py

--- debye.py
+++ debye.py
@@ -1,11 +1,18 @@
 """
 The Debye length is the distance to which a point charge can have its influence felt.
+Written in SI units.
 """
 
 import math
+import numpy as np
+from astropy import units as u
 
-def calculate_debye(electron_nd=1e15, ion_nd=1e15, electron_ke=2e6,
-                    ion_ke=2e6):
+K_BOLTZMANN = 1.3806e-23
+ELECTRON_CHARGE = 1.6021766208e-19
+EPSILON = 8.8541e-12
+
+def calculate_debye(electron_nd=1e15*u.m**-3, ion_nd=1e15*u.m**-3, Te=2e6*u.K,
+                    Ti=2e6*u.K):
     """
     Calculates the Debye length using the formula found in "The Physics of
     Solar Flares" from chapter 3, page 61.
@@ -13,30 +20,33 @@ def calculate_debye(electron_nd=1e15, ion_nd=1e15, electron_ke=2e6,
     Parameters
     ----------
 	    electron_nd : float (optional)
-	        Electron number density
+	        Electron number density (m**-3)
 
-		electron_ke : float (optional)
-		    Electron kinetic energy
+		Te : float (optional)
+		    Electron temperature (K)
 
 		ion_nd : float (optional)
-		    Ion number density
+		    Ion number density (m**-3)
 
-		ion_ke : float (optional)
-		    Ion kinetic energy
+		Ti : float (optional)
+		    Ion temperature (K)
 
     Returns
     -------
         debye : float
             The Debye length (m)
-
     """
-    K_BOLTZMANN = 1.3806e-23
-    ELECTRON_CHARGE = 1.6021766208e-19
-    EPSILON = 8.8541e-12
-    total_ke = (electron_nd*electron_ke + ion_nd*ion_ke)/(electron_nd + ion_nd)
-    print "test"
-    debye = math.sqrt(EPSILON*K_BOLTZMANN*total_ke/(electron_nd*(ELECTRON_CHARGE**2)))
-    return debye
+    electron_nd = electron_nd.to(u.m**-3)
+    ion_nd = ion_nd.to(u.m**-3)
+
+    if Te.unit == 'eV':
+        Te = Te.to(units.K, equivalencies=units.temperature_energy())
+    if Ti.unit == 'ev':
+        Ti = Ti.to(units.K, equivalencies=units.temperature_energy())
+
+    total_ke = (electron_nd.value*Te.value + ion_nd.value*Ti.value)/(electron_nd.value + ion_nd.value)
+    debye = math.sqrt(EPSILON*K_BOLTZMANN*total_ke/(electron_nd.value*ELECTRON_CHARGE**2))
+    return debye * u.m
 
 if __name__ == '__main__':
     print(calculate_debye())
